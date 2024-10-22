dcd7619bc32309428a08dd8ae596312443dfb11a
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-10-22 23:53:45 +0530

    route structure

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ const PORT=6969;
 app.get ('/',(req, res)=>{
     res.json({msg:"hello students!"});
 });
+
+// crud functionality of movies
+
+// R reading
+
+app.get('/movies',()=>{
+
+});
+
+// c 
+
+app.post('/movies',()=>{
+
+});
+
+// E
+
+app.put('/movies/:id',()=>{
+
+});
+
+// D
+app.delete('/movies/:id',()=>{
+
+});
+
+// 
 app.listen(PORT,()=>{
     console.log(`The server is running at http://localhost:${PORT}`);
     
