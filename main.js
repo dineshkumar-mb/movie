479b4e375c238e4ede530ef9830fe211dbb9e64c
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-10-23 00:36:31 +0530

    Route proper formats with npm commands

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 import express from "express";
+
+import moviesRoutes from "./routes/moviesRoutes.js";
 const app =express();
 const PORT=6969;
 app.get ('/',(req, res)=>{
-    res.json({msg:"hello students!"});
+    res.json({msg:"hello students!!"});
 });
 
-// crud functionality of movies
-
-// R reading
-
-app.get('/movies',()=>{
-
-});
-
-// c 
-
-app.post('/movies',()=>{
-
-});
-
-// E
-
-app.put('/movies/:id',()=>{
-
-});
-
-// D
-app.delete('/movies/:id',()=>{
-
-});
-
-// 
+// client-> middleware
+app.use('/movies',moviesRoutes);
 app.listen(PORT,()=>{
     console.log(`The server is running at http://localhost:${PORT}`);
     
-});
+}); 
---
+++ routes/moviesRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+// crud functionality of movies
+
+// R reading
+
+router.get('/',(req,res)=>{
+ res.send("Get all movie list");
+});
+
+// c 
+
+router.post('/',(req,res)=>{
+    res.send("Create a movie ");
+
+});
+
+// E
+
+router.put('/:id',(req,res)=>{
+    res.send("Update a movie ");
+});
+
+// D
+router.delete('/:id',(req,res)=>{
+    res.send("Delete a movie ");
+});
+
+export default router;
